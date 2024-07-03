c58e3f0752bea8704853d3bde2e6a655a7f706c9
SHUBHAM KESHRI <142579892+Shubhamkeshri3905@users.noreply.github.com>
2024-07-03 11:27:36 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./App.css";
+import MainQuiz from "./MainQuiz";
+
+function App() {
+  return (
+    <div className="App">
+      <MainQuiz />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
